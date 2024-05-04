ea9a4e1641c43a104c464c8ad29ece87a95d923e
Arseniy Ogarkov <dev.senya@gmail.com>
2024-05-04 18:21:38 +0300

    04.05.2024

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class Game:
     def move_piece(self):
         # подение куба
         # должно будет работать только если фигура еще падает
-        # if self.cube[-1][1] < self.cup.cup_h - 1:
+        # if self.cube[-1][1] < self.cup.height - 1:
         #     for x in range(len(self.cube)):
         #         point = list(self.cube[x])
         #         point[1] += 1
@@ -49,7 +49,7 @@ class Game:
 
 
         # движение вправо и влево
-        if self.cube[-1][1] < self.cup.cup_h - 1:
+        if self.cube[-1][1] < self.cup.height - 1:
             for x in range(len(self.cube)):
                 point = list(self.cube[x])
                 if point[0] <= 0:
@@ -81,49 +81,67 @@ class Game:
 
 class Cup:
     def __init__(self, cup_h, cup_w, serface, cell_size):
-        self.cup_h, self.cup_w = cup_h, cup_w
+        self.width, self.height = cup_w, cup_h
         self.surface = serface
         self.cell_size = cell_size
-        self.grid = [[0] * self.cup_w for x in range(self.cup_h)]
-        self.posX = (WINDOW_W - cup_w * cell_size) // 2
-        self.posY = WINDOW_H - cup_h * cell_size
-        # [print(x) for x in self.grid]
+        self.gridList = [[0] * self.width for x in range(self.height)]
+        self.posX = (WINDOW_W - self.width * cell_size) // 2
+        self.posY = WINDOW_H - self.height * cell_size
+        # [print(x) for x in self.gridList]
 
     def clear(self):
-        for line in range(len(self.grid)):
-            for col in range(len(self.grid[line])):
-                if self.grid[line][col] == 1:
-                    self.grid[line][col] = 0
+        for line in range(len(self.gridList)):
+            for col in range(len(self.gridList[line])):
+                if self.gridList[line][col] == 1:
+                    self.gridList[line][col] = 0
 
     def draw(self):
         # отрисовка вертикальных линий
-        for x in range(self.cup_w + 1):
-            pygame.draw.line(self.surface, GREY, (self.posX + self.cell_size * x, self.posY),
-                             (self.posX + self.cell_size * x, WINDOW_H))
+        for numLine in range(self.width + 1):
+            startPoint = self.posX + self.cell_size * numLine, self.posY
+            endPoint = self.posX + self.cell_size * numLine, WINDOW_H
+
+            pygame.draw.line(self.surface, GREY, startPoint, endPoint)
         # отрисовка горизонтальных линий
-        for x in range(self.cup_h + 1):
+        for x in range(self.height + 1):
             pygame.draw.line(self.surface, GREY, (self.posX, self.posY + self.cell_size * x),
-                             (self.posX + self.cup_w * self.cell_size, self.posY + self.cell_size * x))
+                             (self.posX + self.width * self.cell_size, self.posY + self.cell_size * x))
         # отрисовка фигур
-        for line in range(len(self.grid)):
+        for line in range(len(self.gridList)):
             # print(f"начало линии {line}")
-            for col in range(len(self.grid[line])):
+            for col in range(len(self.gridList[line])):
                 rect = (self.posX + self.cell_size * col, self.posY + self.cell_size * line, 20, 20)
                 # point2 = (self.posX + self.cell_size + self.cell_size * col, self.posY  + self.cell_size + self.cell_size * line)
                 # print(point1, point2)
-                if self.grid[line][col] == 1:
+                if self.gridList[line][col] == 1:
                     pygame.draw.rect(self.surface, GREEN, rect)
-                elif self.grid[line][col] == 2:
+                elif self.gridList[line][col] == 2:
                     pygame.draw.rect(self.surface, LIGHT_GREEN, rect)
 
-    def set_move_status(self, addr_cube, is_moving=True):  # [(0,0),(1,0),(0,1),(1,1)]
+    # скорее всего это должа делать игра
+    def set_move_status(self, figura, is_moving=True):  # [(0,0),(1,0),(0,1),(1,1)]
         print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         status = 1 if is_moving else 2
-        for cube in addr_cube:
-            self.grid[cube[1]][cube[0]] = status
-        [print(x) for x in self.grid]
+        for cube in figura:
+            self.gridList[cube[1]][cube[0]] = status
+        [print(x) for x in self.gridList]
 
+class Piece:
+    def __init__(self, type, coords):
+        self.type = type
+        self.massBlocks = coords
 
+    def move_right(self):
+        for x in range(len(self.massBlocks)):
+            point = list(self.massBlocks[x])
+            point[0] += 1
+            self.massBlocks[x] = tuple(point)
+
+    def move_left(self):
+        for x in range(len(self.massBlocks)):
+            point = list(self.massBlocks[x])
+            point[0] -= 1
+            self.massBlocks[x] = tuple(point)
 
 if __name__ == "__main__":
     game = Game()
